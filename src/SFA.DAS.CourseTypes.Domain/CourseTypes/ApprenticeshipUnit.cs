f45ae7876c193bec7e36a53ca26c7e08b2e86495
Corey Faulconbridge <corey.faulconbridge@digital.education.gov.uk>
2026-02-27 11:56:12 +0000

    Update min age

--- src/SFA.DAS.CourseTypes.Domain/CourseTypes/ApprenticeshipUnit.cs
+++ src/SFA.DAS.CourseTypes.Domain/CourseTypes/ApprenticeshipUnit.cs
@@ -6,7 +6,7 @@ public class ApprenticeshipUnit : CourseType
 {
     public override string ShortCode => "ApprenticeshipUnit";
     public override RecognitionOfPriorLearning RecognitionOfPriorLearning => new RecognitionOfPriorLearningNotRequired();
-    public override LearnerAge LearnerAge => new(minimumAge: 15, maximumAge: 115);
+    public override LearnerAge LearnerAge => new(minimumAge: 19, maximumAge: 115);
     public override CourseDuration CourseDuration => new(minimumDurationMonths: 8, maximumDurationMonths: 48);
     public override AllowBulkUpload AllowBulkUpload => new(isAllowed: false);
     public override AllowFlexiDeliveryModel AllowFlexiDeliveryModel => new(isAllowed: false);
